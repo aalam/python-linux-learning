3f8b28ecc08f27c31c0648d604b08a6a7bcfd956
A S Alam <apbrar@gmail.com>
2026-01-06 21:50:32 -0600

    2026Jan06-added and updated file with various AI

---
+++ 06day.py
@@ -0,0 +1,15 @@
+#Jan06 - 6th day python
+
+#do exercise - https://labex.io/learn/python
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+--
+- integer, float, string, True False, AND, OR, NOT
+-Converting Between Data Types
