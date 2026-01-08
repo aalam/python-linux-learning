d24d2fb439ef40c7635124d9fd0ee2067bd78de6
A S Alam <apbrar@gmail.com>
2026-01-07 23:22:55 -0600

    2026Jan07 - started with basic System Admin tasks

---
+++ 07day.py
@@ -0,0 +1,27 @@
+#2026Jan07 - system admin
+#https://python-memo.fabrice-salvaire.fr/examples/snippets/sysadmin.html
+import os
+import re
+import subprocess
+import tempfile
+
+
+with tempfile.TemporaryFile() as fh:
+    fh.write(b'Hello world!')
+    fh.seek(0)
+    print(fh.read())
+
+with tempfile.TemporaryDirectory() as temp_directory:
+    print('Created temporary directory', temp_directory)
+
+#Create a filesystem hierarchy
+with tempfile.TemporaryDirectory() as tmp_directory:
+    print('Created temporary directory', tmp_directory)
+    for directory in ('.', 'subdir'):
+        directory_path = os.path.join(tmp_directory, directory)
+        if not os.path.exists(directory_path):
+            os.mkdir(directory_path)
+        for name in ('file1.txt', 'file.txt'):
+            path = os.path.join(directory_path, name)
+            with open(path, 'w') as fh:
+                fh.write('...')
