ca071e4eb57b482d11e6278dc2e10dbd72d3406c
A S Alam <apbrar@gmail.com>
2026-01-09 00:07:53 -0600

    2026Jan08 - file ready with lab work labex.io

---
+++ 08day.py
@@ -0,0 +1,14 @@
+#2026Jan08
+#1 labex  Create an Astronaut Name Tag Processor
+
+#2 https://automatetheboringstuff.com - nice book to read, want to buy one day
+
+#3 Python function parameters and arguments ??
+
+#1 name_tag_processor.py
+name=input("Please enter name")
+name_upper = name.upper()
+name_underscore = name_upper.replace(" ", "_")
+name_prefix = "ASTRONAUT_" + name_underscrore
+print(name)
+print(name_prefix)
