8b2f5cbfaf75268dc70541a709256a945df0847d
A S Alam <apbrar@gmail.com>
2026-01-09 19:25:39 -0600

    2026Jan09 - done with automatetheboringstuff Chap01

---
+++ 09day.py
@@ -0,0 +1,18 @@
+''''
+2003Jan08
+1. Labex - python control structures
+2. A Byte of Python https://python.swaroopch.com
+3. Automate the boring stuff with Python https://automatetheboringstuff.com/3e/chapter0.html
+
+Rectangle Printer completed
+
+'''
+height = input("please provide the column")
+width = input("Please provide the rows")
+print( ("O" * int(width) + "\n") * int(height)) )
+
+
+area = int(height) * int(width)
+perimeter = 2 (int(height) + int(width))
+print("Area of rectangle: " area)
+print("Perimeter of Rectangle:" perimeter)
