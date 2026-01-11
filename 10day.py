78ab35d984e8c492156bf26b95c72b576405b219
A S Alam <apbrar@gmail.com>
2026-01-10 22:48:41 -0600

    2026Jan10 - learned loop - while, if elif, nested loop, break, continue

---
+++ 10day.py
@@ -0,0 +1,17 @@
+
+#https://automatetheboringstuff.com/3e/chapter2.html
+#Chapter 02
+#Boolean + 2 IF-ELSE AND FLOW CONTROL
+
+
+
+---
+Python control structures
+labex.io
+completed for
+if elif else
+while - While loops are often used when you do not know in advance how many times you need to iterate
+ - use Break with while loop
+
+Nested Loops and Loop Control Statements
+continue - which skip the current iteration and move to next
