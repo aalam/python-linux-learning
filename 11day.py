c360523cdbce4d3f714015d390cabf6ea7d4e362
A S Alam <apbrar@gmail.com>
2026-01-11 23:10:35 -0600

    2026Jan11 - Labex.io - function and modules - package module

---
+++ 11day.py
@@ -0,0 +1,16 @@
+2026Jan11 - Create a Rocket Launch Countdown on the Labex.io
+
+#countdown.py - for i in range (10, 0, -1)
+
+#Python Functions and Modules 
+# - Understanding Function Scope inner function, global, change the global
+# - Modules
+'''    Python compiles it into bytecode and stores the compiled code in a __pycache__ directory. This directory is created in the same location as the module file and contains compiled bytecode files (.pyc or .pyo).
+
+- Importing Specific Functions from Modules
+how to import specific functions and use aliases (with the as keyword).
+-  Creating a Package
+    __init__.py file is required to make Python treat the directory as a package. It can be empty or contain initialization code for the package.
+
+'''
+
