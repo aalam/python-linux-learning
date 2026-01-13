95214ea51b72d9a4b204eff489c2fb5f5371f266
A S Alam <apbrar@gmail.com>
2026-01-12 22:08:01 -0600

    2026Jan12 - Date structure - list, tuple, set, dictionary

---
+++ 12day.py
@@ -0,0 +1,28 @@
+#2026Jan12 - [200~Space Mission Calculator
+- def function, call it in another file
+-use grav_force =  gravity_force(planet_mass, spacecraft_mass, distance)
+- Rounding: Ensure all your output values are formatted to two decimal places (e.g., using :.2f in an f-string).
+
+- print(f"Fuel needed: {fuel_needed:.2f} liters")
+
+#Python Data Structures -  lists, tuples, sets, and dictionaries
+-list - append(), insert()
+    To remove items, you can use the remove() method or the del statement
+
+-Tuples: Tuples are created using parentheses ()
+
+-SET which are unordered collections of unique elements in Python.
+    Sets are created using curly braces {} or the set() function. 
+    Note that the order of elements may vary when you print a set.
+    Sets automatically remove duplicate elements
+    Sets support mathematical set operations like union, intersection, and difference
+    You can check if an element is in a set using the in keyword
+
+-Dictionaries, which are unordered collections of key-value pairs in Python:
+    Dictionaries are created using curly braces {} with key-value pairs separated by colons
+    Dictionaries are mutable, so you can add or modify key-value pairs:
+    To remove a key-value pair, use the del statement:
+    You can check if a key exists in a dictionary using the in keyword:
+    Dictionaries have useful methods like keys(), values(), and items():
+    You can use dictionary comprehension to create dictionaries concisely:
+    Dictionaries are extremely useful for storing and retrieving data with unique keys.
