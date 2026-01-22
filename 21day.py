b26476c6d85fbd1f9b0beddb517d42ab2530de90
A S Alam <apbrar@gmail.com>
2026-01-21 22:51:11 -0600

    2026Jan21 - Python Keywords and built-in - syntax erros

---
+++ 21day.py
@@ -0,0 +1,6 @@
+#2026Jan21 - Keywords and Built-in Identifiers in Python
+- keyword - import keyword
+- Built-in Identifiers - print(), int, str
+
+ -- Python does not prevent you from reassigning built-in identifiers
+
