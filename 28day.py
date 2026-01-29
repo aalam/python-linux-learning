53bdc135e70949afc1c035622d5dcff8eb35a673
A S Alam <apbrar@gmail.com>
2026-01-28 22:56:31 -0600

    2026Jan28 - fucntion

---
+++ 28day.py
@@ -0,0 +1,8 @@
+2026Jan28 - Learning Loop ad Range() function again
+#https://inventwithpython.com/automate3workbook/chapter3.html
+- for loop can execute a block of code a specific number of times with the range() function
+- range() function with two or three arguments, in which case the first two arguments are the start and stop values, and the third is the step argument, or the amount by which the variable is increased after each iteration.
+
+#https://inventwithpython.com/automate3workbook/chapter4.html
+- Functions
+
