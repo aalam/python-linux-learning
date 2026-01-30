878e60d5d9a7e45947c1aa0e12667cf24f74a352
A S Alam <apbrar@gmail.com>
2026-01-30 07:06:10 -0600

    2026Jan29 - learned about debugging from inventwithpython - automate3workbook

---
+++ 29day.py
@@ -0,0 +1,11 @@
+2026Jan29 - 
+#https://inventwithpython.com/automate3workbook/chapter5.html
+- DEBUGGING
+
+
+-  Know how to make assertions with assert statements.
+- Understand the difference between exceptions and assertions and the roles they play.
+- Use the logging module to create a trail of clues regarding what your program is doing, and in what order.
+- Be able to run your programs under the debugger to identify what is happening behind the scenes.
+- Use debugger features such as breakpoints, and inspect the values stored in variables.
+
